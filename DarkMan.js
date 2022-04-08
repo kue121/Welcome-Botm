76f36a85500a6ccc42d4e7d5953178a9d8a60ea8
kue121 <102326068+kue121@users.noreply.github.com>
2022-04-08 18:52:43 +0300

    Update DarkMan.js

--- DarkMan.js
+++ DarkMan.js
@@ -25,7 +25,7 @@ const DarkMan = new Discord.Client();
 const prefix = "D!";
 
 
-DarkMan.login("");
+DarkMan.login("OTYyMDEwMTk2OTY2Nzg5MTkw.YlBT0A.N-IGTYFQegORYPvBOapyvUnxHcM");
 DarkMan.on("ready", async () => {
   console.log(`Logged in as ${DarkMan.user.username}!`);
   DarkMan.user.setStatus("ONLINE");
